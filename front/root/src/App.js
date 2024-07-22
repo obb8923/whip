e9eae0e1742f50c9696cd724a382e09d7529a3fe
intelligent04 <87917946+intelligent04@users.noreply.github.com>
2024-07-22 15:38:54 +0900

    세팅 중

--- front/root/src/App.js
+++ front/root/src/App.js
@@ -1,24 +1,17 @@
 import React from "react";
 import Main from "./components/Main";
 import CalendarPage from "./components/CalendarPage";
+import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <AppContent />
-    </BrowserRouter>
-  );
-}
-
-function AppContent() {
-  return (
-    <>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
