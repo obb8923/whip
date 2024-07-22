7adc04c6b87891eb7f7cb22f088c67e59ca8dee6
intelligent04 <87917946+intelligent04@users.noreply.github.com>
2024-07-23 00:04:18 +0900

    Revert "Merge branch 'main' of https://github.com/LLMbootcamp-KMU/diet"
    This reverts commit 2526dd88c85d664b921873e3c9d274e8ab25d361, reversing
    changes made to da39d176c44c94e256792338fb74837cf86ed1ff.

--- front/root/src/App.js
+++ front/root/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Main from "./components/home/Main";
 import CalendarPage from "./components/CalendarPage";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,8 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
