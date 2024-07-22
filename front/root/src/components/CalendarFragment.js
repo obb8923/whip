3148588fafb61fe2397284a5309a020fbc439bcd
JeongBin Park <obb8923@naver.com>
2024-07-23 00:10:45 +0900

    달력

--- front/root/src/components/CalendarFragment.js
+++ front/root/src/components/CalendarFragment.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css"; // 기본 스타일을 포함시킵니다.
+import moment from "moment";
+import "react-calendar/dist/Calendar.css"; // 달력 기본 스타일을 포함
 
 function App() {
   const [date, setDate] = useState(new Date());
@@ -37,6 +38,10 @@ function App() {
         tileContent={tileContent}
         onClickDay={handleClickDay}
         tileClassName={tileClassName}
+        formatDay={(locale, date) => moment(date).format("DD")}
+        minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+        maxDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
+        showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
       />
       <p>선택한 날짜: {date.toDateString()}</p>
     </div>
