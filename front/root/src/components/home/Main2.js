f4ad671bf1f2f37dc59f9e47a833c759338d701d
intelligent04 <87917946+intelligent04@users.noreply.github.com>
2024-07-22 18:12:18 +0900

    입력창 만듦

--- front/root/src/components/home/Main2.js
+++ front/root/src/components/home/Main2.js
@@ -46,27 +46,34 @@ export default function Main() {
         <form className={styles.formContainer}>
           <div className={styles.relativeGroup}>
             <input type="email" name="floating_email" id="floating_email" className={styles.inputField} placeholder=" " required />
-            <label htmlFor="floating_email" className={styles.label}>날짜</label>
+            <label htmlFor="floating_email" className={styles.label}>Email address</label>
           </div>
           <div className={styles.relativeGroup}>
             <input type="password" name="floating_password" id="floating_password" className={styles.inputField} placeholder=" " required />
-            <label htmlFor="floating_password" className={styles.label}>칼로리</label>
+            <label htmlFor="floating_password" className={styles.label}>Password</label>
           </div>
           <div className={styles.relativeGroup}>
             <input type="password" name="repeat_password" id="floating_repeat_password" className={styles.inputField} placeholder=" " required />
-            <label htmlFor="floating_repeat_password" className={styles.label}>탄수화물</label>
+            <label htmlFor="floating_repeat_password" className={styles.label}>Confirm password</label>
           </div>
-          <div className={styles.relativeGroup}>
-            <input type="password" name="repeat_password" id="floating_repeat_password" className={styles.inputField} placeholder=" " required />
-            <label htmlFor="floating_repeat_password" className={styles.label}>단백질</label>
-          </div>
-          <div className={styles.relativeGroup}>
-            <input type="password" name="repeat_password" id="floating_repeat_password" className={styles.inputField} placeholder=" " required />
-            <label htmlFor="floating_repeat_password" className={styles.label}>지방</label>
-          </div>
-          
           <div className={styles.gridGroup}>
             <div className={styles.relativeGroup}>
+              <input type="text" name="floating_first_name" id="floating_first_name" className={styles.inputField} placeholder=" " required />
+              <label htmlFor="floating_first_name" className={styles.label}>First name</label>
+            </div>
+            <div className={styles.relativeGroup}>
+              <input type="text" name="floating_last_name" id="floating_last_name" className={styles.inputField} placeholder=" " required />
+              <label htmlFor="floating_last_name" className={styles.label}>Last name</label>
+            </div>
+          </div>
+          <div className={styles.gridGroup}>
+            <div className={styles.relativeGroup}>
+              <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" className={styles.inputField} placeholder=" " required />
+              <label htmlFor="floating_phone" className={styles.label}>Phone number (123-456-7890)</label>
+            </div>
+            <div className={styles.relativeGroup}>
+              <input type="text" name="floating_company" id="floating_company" className={styles.inputField} placeholder=" " required />
+              <label htmlFor="floating_company" className={styles.label}>Company (Ex. Google)</label>
             </div>
           </div>
           <button type="submit" className={styles.submitButton}>Submit</button>
