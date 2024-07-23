d2e82cde4ba28c9c7d7aa2dab795f3f80993cba0
JeongBin Park <obb8923@naver.com>
2024-07-24 00:18:48 +0900

    Main 스타일

--- front/root/src/components/home/Main.js
+++ front/root/src/components/home/Main.js
@@ -13,6 +13,7 @@ export default function Main() {
   const [protein, setProtein] = useState("");
   const [fat, setFat] = useState("");
 
+  // 로그인 안 되어있을 때, 로그인 화면으로 보내기
   useEffect(() => {
     if (!localStorage.getItem("id")) {
       navigate("/login");
@@ -26,11 +27,16 @@ export default function Main() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const user_id = localStorage.getItem("id");
-    console.log("Submitting search with text:", user_id, "and food name:", searchText); // Debug log
+    console.log(
+      "Submitting search with text:",
+      user_id,
+      "and food name:",
+      searchText
+    ); // Debug log
     try {
       const response = await axios.post("http://localhost:3000/back/api/send", {
         user_id,
-        food_name: searchText
+        food_name: searchText,
       });
       console.log("Received response:", response.data);
       const { calorie, carbohydrate, protein, fat, food_name } = response.data;
@@ -40,135 +46,153 @@ export default function Main() {
       setFat(fat);
       setFoodName(food_name);
     } catch (error) {
-      console.error("Error submitting search:", error.response?.data || error.message);
+      console.error(
+        "Error submitting search:",
+        error.response?.data || error.message
+      );
     }
   };
 
   return (
     <div className="frameBox">
       <div className="contentBox">
-        <div className={styles.frameBox}>
-          <div className={styles.contentBox}>
-            <form className={styles.formContainer} onSubmit={handleSubmit}>
-              <label htmlFor="defaultSearch" className={styles.srOnly}>
-                Search
-              </label>
-              <div className={styles.inputContainer}>
-                <div className={styles.iconContainer}>
+        <div className={styles.mainFrame}>
+          <form className={styles.formContainer} onSubmit={handleSubmit}>
+            <label htmlFor="defaultSearch" className={styles.srOnly}>
+              Search
+            </label>
+            <div className={styles.inputContainer}>
+              <div className={styles.iconContainer}>
+                <svg
+                  className={styles.icon}
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <div className={styles.searchBox}>
+                <input
+                  type="search"
+                  id="defaultSearch"
+                  className={styles.searchInput}
+                  placeholder="닭가슴살 2개를 먹었어"
+                  value={searchText}
+                  onChange={handleInputChange}
+                  required
+                />
+
+                <button type="submit" className={styles.searchButton}>
                   <svg
-                    className={styles.icon}
+                    className={styles.iconRA}
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
-                    viewBox="0 0 20 20"
+                    viewBox="0 0 14 10"
                   >
                     <path
                       stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
                     />
                   </svg>
-                </div>
-                <div className={styles.searchBox}>
-                  <input
-                    type="search"
-                    id="defaultSearch"
-                    className={styles.searchInput}
-                    placeholder="예시: 닥터유 에너지바 2개를 먹었어"
-                    value={searchText}
-                    onChange={handleInputChange}
-                    required
-                  />
-                  <button type="submit" className={styles.searchButton}>
-                    Search
-                  </button>
-                </div>
+                  <span className={styles.srOOnly}>Icon description</span>
+                </button>
               </div>
-            </form>
-            <form className={styles.formContainer}>
-              <div className={styles.relativeGroup}>
-                <input
-                  type="text"
-                  name="foodName"
-                  id="foodName"
-                  className={styles.inputField}
-                  placeholder=" "
-                  value={foodName}
-                  readOnly
-                  required
-                />
-                <label htmlFor="foodName" className={styles.label}>
-                  음식이름
-                </label>
-              </div>
-              <div className={styles.relativeGroup}>
-                <input
-                  type="text"
-                  name="cal"
-                  id="cal"
-                  className={styles.inputField}
-                  placeholder=" "
-                  value={calorie}
-                  readOnly
-                  required
-                />
-                <label htmlFor="cal" className={styles.label}>
-                  칼로리
-                </label>
-              </div>
-              <div className={styles.relativeGroup}>
-                <input
-                  type="text"
-                  name="carbo"
-                  id="carbo"
-                  className={styles.inputField}
-                  placeholder=" "
-                  value={carbohydrate}
-                  readOnly
-                  required
-                />
-                <label htmlFor="carbo" className={styles.label}>
-                  탄수화물
-                </label>
-              </div>
-              <div className={styles.relativeGroup}>
-                <input
-                  type="text"
-                  name="protein"
-                  id="protein"
-                  className={styles.inputField}
-                  placeholder=" "
-                  value={protein}
-                  readOnly
-                  required
-                />
-                <label htmlFor="protein" className={styles.label}>
-                  단백질
-                </label>
-              </div>
-              <div className={styles.relativeGroup}>
-                <input
-                  type="text"
-                  name="fat"
-                  id="fat"
-                  className={styles.inputField}
-                  placeholder=" "
-                  value={fat}
-                  readOnly
-                  required
-                />
-                <label htmlFor="fat" className={styles.label}>
-                  지방
-                </label>
-              </div>
-              
-              <button className={styles.submitButton} onClick={handleSubmit}>
-                Submit
-              </button>
-            </form>
-          </div>
+            </div>
+          </form>
+
+          <form className={styles.formContainer}>
+            <div className={styles.inputGroup}>
+              <input
+                type="text"
+                name="foodName"
+                id="foodName"
+                className={styles.inputField}
+                placeholder=" "
+                value={foodName}
+                readOnly
+                required
+              />
+              <label htmlFor="foodName" className={styles.inputLabel}>
+                음식이름
+              </label>
+            </div>
+            <div className={styles.inputGroup}>
+              <input
+                type="text"
+                name="cal"
+                id="cal"
+                className={styles.inputField}
+                placeholder=" "
+                value={calorie}
+                readOnly
+                required
+              />
+              <label htmlFor="cal" className={styles.inputLabel}>
+                칼로리
+              </label>
+            </div>
+            <div className={styles.inputGroup}>
+              <input
+                type="text"
+                name="carbo"
+                id="carbo"
+                className={styles.inputField}
+                placeholder=" "
+                value={carbohydrate}
+                readOnly
+                required
+              />
+              <label htmlFor="carbo" className={styles.inputLabel}>
+                탄수화물
+              </label>
+            </div>
+            <div className={styles.inputGroup}>
+              <input
+                type="text"
+                name="protein"
+                id="protein"
+                className={styles.inputField}
+                placeholder=" "
+                value={protein}
+                readOnly
+                required
+              />
+              <label htmlFor="protein" className={styles.inputLabel}>
+                단백질
+              </label>
+            </div>
+            <div className={styles.inputGroup}>
+              <input
+                type="text"
+                name="fat"
+                id="fat"
+                className={styles.inputField}
+                placeholder=" "
+                value={fat}
+                readOnly
+                required
+              />
+              <label htmlFor="fat" className={styles.inputLabel}>
+                지방
+              </label>
+            </div>
+
+            <button className={styles.searchButton} onClick={handleSubmit}>
+              Submit
+            </button>
+          </form>
         </div>
       </div>
       <GNB />
